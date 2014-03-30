2346bdaff243d338d5b13a5b1893a49ebd44ecdb
williamlfang <william.lian.fang@gmail.com>
2014-03-30 23:37:37 +0800

    update

---
+++ auto2.sh
@@ -0,0 +1,9 @@
+cd /home/william/williamlfang.github.com        
+git add . -A           
+git commit -m "update"       
+
+git remote rm origin   
+git remote add origin git@github.com:williamlfang/williamlfang.github.com.git
+git push origin master
+
+jekyll --server       
